725f98eede265c427cb351a1698ac37976f13415
rafal-gasior <rafalgas91@gmail.com>
2019-09-07 10:11:40 +0200

    Add animations and styles

--- src/app/directives/color.directive.ts
+++ src/app/directives/color.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, ElementRef, OnInit} from '@angular/core';
+import { Directive, Input, ElementRef, OnInit, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[colorChange]'
@@ -20,6 +20,16 @@ export class ColorDirective implements OnInit {
       this.el.nativeElement.classList.add(this.plusColor);
         this.el.nativeElement.classList.remove(this.minusColor);
     }
+  }
+
+  @HostListener('mouseover') onMouseOver(){
+      //this.el.nativeElement.style.boxShadow = "6px 6px 5px #999";
+      this.el.nativeElement.style.transform = "translateY(10px)";
+  }
+
+  @HostListener('mouseout') onMouseOut(){
+    //  this.el.nativeElement.style.boxShadow = "";
+      this.el.nativeElement.style.transform = "";
   }
 
 }
